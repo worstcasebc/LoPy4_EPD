dfd063ae45fbd3324647d6bc73bab0c5e22f079d
worstcasebc <58089458+worstcasebc@users.noreply.github.com>
2019-12-15 18:14:22 +0100

    moved epd-settings back to main.py

--- config.py
+++ config.py
@@ -3,14 +3,6 @@
 WiFiSSID = "<yourSSIDhere>"
 WiFiKey  = "<yourKEYhere>"
 
-# which e-paper display to use
-#epd_type = "epd2in7"
-epd_type = "epd4in2"
-#epd_type = "epd7in5bc_bw"       # use only black&white layer
-#epd_type = "epd7in5bc_color"    # also use yellow/red layer
-
-epd_rotation = 1
-
 # Mapping e-Paper HAT to LoPy4 as Pxx (and Expansion board Gxx)
 RST_PIN  = 'P19'    #G6
 DC_PIN   = 'P20'    #G7
